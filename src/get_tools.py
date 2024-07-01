89b366bbe662e58d96e5b54ae5fc921f21e6a110
Ashad Qureshi <ashad001sp@gmail.com>
2024-07-01 17:11:21 +0500

    agent is on

--- src/get_tools.py
+++ src/get_tools.py
@@ -1,4 +1,5 @@
 import os
+import json
 from pathlib import Path
 from dotenv import load_dotenv
 from src.data_ingestion import QueryTools
@@ -6,18 +7,33 @@ from src.data_ingestion import QueryTools
 def get_all_tools():
     load_dotenv()
 
+    if os.path.exists("tools.json"):
+        with open("tools.json", "r") as f:
+            paper_to_tools_dict = json.load(f)
+            all_tools = [t for paper in paper_to_tools_dict for t in paper_to_tools_dict[paper]]
+            return all_tools
+
     papers = os.listdir('./data')
 
-    paper_to_tools_dict = {}
-    for paper in papers:
-        print(f"Getting tools for paper: {paper}")
-        tools = QueryTools(f"./data/{paper}", Path(paper).stem)
-        vector_tool, summary_tool = tools.get_query_tools()
-        paper_to_tools_dict[paper] = [vector_tool, summary_tool]
+    try:
+        paper_to_tools_dict = {}
+        for paper in papers:
+            try:
+                print(f"Getting tools for paper: {paper}")
+                tools = QueryTools(f"./data/{paper}", Path(paper).stem)
+                vector_tool, summary_tool = tools.get_query_tools()
+                paper_to_tools_dict[paper] = [vector_tool, summary_tool]
+            except Exception as e:
+                print(f"Error occurred: {str(e)} in paper: {paper}")
+    except Exception as e:
+        print(f"Error occurred: {str(e)}")
 
     print("Done!")
 
     all_tools = [t for paper in papers for t in paper_to_tools_dict[paper]]
+    # Save tools to a file
+    with open("tools.json", "w") as f:
+        json.dump(paper_to_tools_dict, f, indent=4, default=str)
     return all_tools
 
 # all_tools = get_all_tools()
---
+++ src/main.py
@@ -0,0 +1,39 @@
+from llama_index.core import VectorStoreIndex
+from llama_index.core.objects import ObjectIndex
+from llama_index.core.agent import FunctionCallingAgentWorker
+from llama_index.core.agent import AgentRunner
+from src.get_tools import get_all_tools 
+from llama_index.llms.openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+class ChatAgent:
+    def __init__(self):
+        load_dotenv()
+        self.all_tools = get_all_tools()
+        self.llm = OpenAI(model="gpt-3.5-turbo")
+        self.obj_index = ObjectIndex.from_objects(
+            self.all_tools,
+            index_cls=VectorStoreIndex,
+        )
+        self.obj_retriever = self.obj_index.as_retriever(similarity_top_k=3)
+        self.agent_worker = FunctionCallingAgentWorker.from_tools(
+            tool_retriever=self.obj_retriever,
+            llm=self.llm, 
+            system_prompt=""" \
+            You are an agent designed to answer queries over a set of given papers.
+            Please always use the tools provided to answer a question. Do not rely on prior knowledge.\
+
+            """,
+            verbose=True
+        )
+        self.agent = AgentRunner(self.agent_worker)
+
+    def query(self, question):
+        response = self.agent.query(question)
+        return str(response)
+
+# 
+agent = ChatAgent()
+response = agent.query("What is the Artificial Intelligence for Climate Action Initiative?")
+print(response)
