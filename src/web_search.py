26e20f08646dc989793ddf89ad4c538aaf022358
Ashad Qureshi <ashad001sp@gmail.com>
2024-07-07 13:02:39 +0500

    web search pipeline

--- src/web_search.py
+++ src/web_search.py
@@ -29,7 +29,7 @@ class WebSearch:
     
     def save_results(self):
         for i, result in enumerate(self.search_results):
-            with open(f"./data/search_result_{i}.html", "w") as f:
+            with open(f"./data/search_result_{i}.txt", "w", encoding="utf-8") as f:
                 f.write(result)
                 
     def ingest(self):
@@ -42,16 +42,33 @@ class WebSearch:
             print(f"Error occurred: {str(e)}")
             
     def index(self):
-        self.vector_index = VectorStoreIndex(self.documents, self.embed_model)
-        self.search_query = self.vector_index.as_query_engine()
+        self.vector_index = VectorStoreIndex.from_documents(self.documents)
+        self.sq = self.vector_index.as_query_engine()
         
     def search_query(self, query: str) -> List[str]:
-        return self.search_query.query(query)
+        return self.sq.query(query)
+    
+    def reset(self):
+        for file in os.listdir("./data"):
+            os.remove(f"./data/{file}")
+        self.search_results = []
+        self.documents = []
+        self.vector_index = None
+        self.sq = None
+        
+    def feed(self, urls: List[str]):
+        self.feed_urls(urls)
+        self.search()
+        self.save_results()
+        self.ingest()
+        self.index()
     
 
 if __name__ == "__main__":
     ws = WebSearch()
-    ws.feed_urls(["https://blog.jina.ai"])
-    ws.ingest()
-    ws.index()
-    print(ws.search_query("llama"))
+    ws.reset()
+    ws.feed(urls=['https://github.com/Ashad001'])
+    res = ws.search_query("what Pinned projects does he have and what stack has he worked on?")
+    print(res)
+    with open("search_results.txt", "w") as f:
+        f.write(str(res))
